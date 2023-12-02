5fe6041867bb18164e69e8af1d052d03d263e969
Victor Ola <victoluolatunji@gmail.com>
2023-12-02 20:08:45 +0100

    ,,,

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,98 @@
-import { Link, NavLink } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+  useLocation,
+} from 'react-router-dom';
+import scroll from './animation/scroll';
+import { useEffect } from 'react';
+import AnimatedCursor from 'react-animated-cursor';
 
-import { Button, Input } from './components';
-export default function Home() {
-  return (
-    <>
-      <main>
-        <Hero />
-        <VideoSection />
-      </main>
-    </>
-  );
-}
+const Root = () => {
+  const { pathname } = useLocation();
 
-const Hero = () => {
-  function handleSubmit(e) {
-    e.preventDefault();
+  // const noLayoutRoutes = ['/login', '/register'];
+
+  const isNoLayout = noLayoutRoutes.some((nlr) => nlr === pathname);
+
+  if (isNoLayout) {
+    return (
+      <div>
+        <ScrollRestoration />
+        <Outlet />
+      </div>
+    );
   }
+
   return (
-    <>
-      <section className='lg:pl-[160px] lg:pb-[195px] lg:pt-[72px]  bg-hero bg-no-repea relative'>
-        <header>
-          <nav className=''>
-            <Link to='/'>
-              <img
-                src='/images/afen.png'
-                alt='logoIcon'
-                draggable={false}
-                className='active:scale-95 transition-all duration-300'
-              />
-            </Link>
-          </nav>
-        </header>
-        <div className='flex flex-col items-start'>
-          <h3
-            style={{ fontFamily: 'Cocogoose Classic Trial, sans-serif' }}
-            className='mt-[250px] font-extrabold lg:leading-[69.6px] text-secondary lg:max-w-[627px] lg:text-[50px]   '
-          >
-            Building the Future, One Block at a Time…
-          </h3>
-          <p className='text-tet font-normal lg:leading-[42px] lg:text-[28px] max-w-[692px] mt-4'>
-            At AFEN, we are dedicated to creating a dynamic global ecosystem
-            that fosters disruptive innovation and community advancement.
-          </p>
-          <p className='text-tet font-normal lg:leading-[42px] lg:text-[28px] max-w-[442px] mt-10'>
-            Be one of the first to receive our newest updates.
-          </p>
-          <form
-            onSubmit={handleSubmit}
-            className='flex flex-row gap-[14px] items-center mt-4'
-          >
-            <fieldset>
-              <Input placeholder='Type your email here' type='email' />
-            </fieldset>
-            <Button className='rounded-[8px] lg:px-[26px] lg:py-4'>
-              Join now !!
-            </Button>
-          </form>
-        </div>
-      </section>
-    </>
+    <div>
+      <ScrollRestoration />
+      <Outlet />
+    </div>
   );
 };
 
-function VideoSection() {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '*',
+        element: <$404 />,
+      },
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  useEffect(() => {
+    // scroll();
+  }, []);
+
   return (
-    <section className='pl-[108px] flex flex-row bg-white lg:py-[120px]'>
-      <div className='bg-[#C4C4C4] w-[702px] h-[496px]'></div>
-    </section>
+    <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={44}
+        color='255, 160, 233'
+        outerAlpha={0.3}
+        innerScale={0.7}
+        outerScale={1.2}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
+          {
+            target: '.custom',
+            options: {
+              innerSize: 12,
+              outerSize: 12,
+              color: '255, 255, 255',
+              outerAlpha: 0.3,
+              innerScale: 0.7,
+              outerScale: 5,
+            },
+          },
+        ]}
+      />
+
+      <RouterProvider router={router} />
+    </>
   );
 }
+
+export default App;
