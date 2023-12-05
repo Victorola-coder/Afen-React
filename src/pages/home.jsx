28d7db951f11ed592d88c7547bce2f03838b6a4f
Destiny Felix Kiisi <kiisifelix06@gmail.com>
2023-12-05 11:25:35 +0100

    feat/mail-chimp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -61,17 +61,43 @@ const Form = () => {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='flex flex-col lg:flex-row gap-[14px] mt-4 w-full max-w-[550px]'
-    >
-      <fieldset className='flex-1'>
-        <Input placeholder='Type your email here' type='email' />
-      </fieldset>
-      <Button className='rounded-[8px] px-[26px] py-[16px] lg:py-4 max-w-max'>
-        Join now !!
-      </Button>
+    // <form
+    //   onSubmit={handleSubmit}
+    //   className='flex flex-col lg:flex-row gap-[14px] mt-4 w-full max-w-[550px]'
+    // >
+    //   <fieldset className='flex-1'>
+    //     <Input placeholder='Type your email here' type='email' />
+    //   </fieldset>
+    //   <Button className='rounded-[8px] px-[26px] py-[16px] lg:py-4 max-w-max'>
+    //     Join now !!
+    //   </Button>
+    // </form>
+    <div id="mc_embed_shell">
+  <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
+  <style type="text/css" dangerouslySetInnerHTML={{__html: "\n          #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}\n        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.\n           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */\n        " }} />
+  <div id="mc_embed_signup">
+    <form action="https://afengroup.us21.list-manage.com/subscribe/post?u=a650f227e4adcdf9438f0a7f8&id=8d601b3ba0&f_id=00e4ece6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+      <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+        <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+        <div className="mc-field-group"><label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label><input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required defaultValue /><span id="mce-EMAIL-HELPERTEXT" className="helper_text" /></div>
+        <div id="mce-responses" className="clear foot">
+          <div className="response" id="mce-error-response" style={{display: 'none'}} />
+          <div className="response" id="mce-success-response" style={{display: 'none'}} />
+        </div>
+        <div aria-hidden="true" style={{position: 'absolute', left: '-5000px'}}>
+          <input type="text" name="b_a650f227e4adcdf9438f0a7f8_8d601b3ba0" tabIndex={-1} defaultValue />
+        </div>
+        <div className="optionalParent">
+          <div className="clear foot">
+            <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" defaultValue="Subscribe" />
+            <p style={{margin: '0px auto'}}><a href="http://eepurl.com/iFsUPM" title="Mailchimp - email marketing made easy and fun"><span style={{display: 'inline-block', backgroundColor: 'transparent', borderRadius: 4}}><img className="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style={{width: 220, height: 40, display: 'flex', padding: '2px 0px', justifyContent: 'center', alignItems: 'center'}} /></span></a></p>
+          </div>
+        </div>
+      </div>
     </form>
+  </div>
+</div>
+
   );
 };
 
